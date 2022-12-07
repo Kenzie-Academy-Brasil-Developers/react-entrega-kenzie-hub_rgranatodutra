3ba8400b80018a7bded314639b49b8bc507c1847
rgranatodutra <r.granatodutra@gmail.com>
2022-12-06 22:08:54 -0300

    feat: toastify

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,27 @@ import { LoginPage } from "./components/pages/Login";
 import { RegisterPage } from "./components/pages/Register";
 import { MainPage } from "./components/pages/Main";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthPage } from "./components/pages/Auth";
+
 export const App = () => {
-
-  const [theme, setTheme] = useState('dark');
-
   return (
     <div className="App">
-      <GlobalStyles theme={theme} />
+      <GlobalStyles theme="dark" />
       <Routes>
+        <Route path="/" element={<AuthPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/app" element={<MainPage />} />
       </Routes>
+      <ToastContainer
+        autoClose={5000}
+        newestOnTop={true}
+        closeOnClick
+        theme="light"
+        pauseOnFocusLoss={false}
+      />
     </div>
   );
-}
+};
