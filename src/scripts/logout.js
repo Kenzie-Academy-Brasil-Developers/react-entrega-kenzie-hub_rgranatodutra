3453c8281a96210182eba7a248ab1ce811e6f302
rgranatodutra <r.granatodutra@gmail.com>
2022-12-13 13:26:18 -0300

    refactor: change api file location

--- src/scripts/logout.js
+++
@@ -1,5 +0,0 @@
-export function logout() {
-    localStorage.removeItem('@rgranatodutra/KenzieHub:userID');
-    localStorage.removeItem('@rgranatodutra/KenzieHub:authToken');
-    window.location.assign('/login');
-};
