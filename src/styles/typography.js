4ceca9e670279e42b32fb8a7641a0da147e449e8
rgranatodutra <r.granatodutra@gmail.com>
2022-12-15 13:53:22 -0300

    fix: responsivity

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,45 +1,47 @@
 import { css } from "styled-components";
 
+
+
 export const typography = {
     title1: css`
-        font-size: 1.125rem;
+        font-size: var(--fs_title-1);
         font-weight: 700;
         line-height: 1.75rem;
     `,
     title2: css`
-        font-size: 1rem;
+        font-size: var(--fs_title-2);
         font-weight: 600;
         line-height: 1.625rem;
     `,
     title3: css`
-        font-size: 0.875rem;
+        font-size: var(--fs_title-3);
         font-weight: 700;
         line-height: 1.5rem;
     `,
     bigline: css`
-        font-size:   1rem;
+        font-size: var(--fs_title-3);
         font-weight: 400;
         line-height: 1.5rem;
     `
     ,
     headline: css`
-        font-size:   0.75rem;
+        font-size: var(--fs_headline);
         font-weight: 400;
         line-height: 1.375rem;
     `,
     headlineB: css`
-        font-size:   0.75rem;
+        font-size: var(--fs_headline);
         font-weight: 600;
         line-height: 1.375rem;
     `,
     headlineI: css`
         font-style:  italic;
-        font-size:   0.75rem;
+        font-size: var(--fs_headline);
         font-weight: 400;
         line-height: 1.375rem;
     `,
     smallB: css`
-        font-size:   0.625rem;
+        font-size: var(--fs_small);
         font-weight: 600;
         line-height: 1.25rem;
     `,
